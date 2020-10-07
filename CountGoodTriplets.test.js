bad1aa55c24cf5ca59f5c2b04aa561e2e94ed7f2
Kyle <kpete2017@gmail.com>
2020-10-07 17:42:57 -0600

    Finished valid threes question

--- CountGoodTriplets.test.js
+++ CountGoodTriplets.test.js
@@ -1,5 +1,24 @@
 const countGoodTriplets = require('./CountGoodTriplets');
 
+const arr = [3,0,1,1,9,7];
+const a = 7;
+const b = 2;
+const c = 3;
+
+const output = 4;
+
+
 test("checks whether the pair of triplets are good", () => {
-    expect().toStrictEqual();
+    expect(countGoodTriplets(arr, a, b, c)).toStrictEqual(output);
+})
+
+const arr2 = [1,1,2,2,3];
+const a2 = 0;
+const b2 = 0;
+const c2 = 1;
+
+const output2 = 0;
+
+test("checks whether the pair of triplets are good", () => {
+    expect(countGoodTriplets(arr2, a2, b2, c2)).toStrictEqual(output2);
 })
